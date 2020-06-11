72f9f6d231b4ed05cd1c40b33f4729355a99f4fe
rexijohn <55958005+rexijohn@users.noreply.github.com>
2020-06-10 21:39:06 -0700

    commit

--- src/pck1/cl1.java
+++ src/pck1/cl1.java
@@ -2,6 +2,7 @@ package pck1;
 //Adding in master
 //Adding from local branch
 //Added from Second Branch
+//Adding from remote server to second branch
 public class cl1 {
 
 	public static void main(String[] args) {
