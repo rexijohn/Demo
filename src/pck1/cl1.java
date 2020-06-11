d67b1d39a51001d6ebe0954babf61eff95dfb140
rexijohn <rexikj@gmail.com>
2020-06-10 22:31:03 -0700

    c

--- src/pck1/cl1.java
+++ src/pck1/cl1.java
@@ -2,7 +2,7 @@ package pck1;
 //Adding in master
 //Adding from local branch
 //Added from Second Branch
-//Adding from remote server to second branch
+//Adding from remote server to second branchs
 public class cl1 {
 
 	public static void main(String[] args) {
