eff1187388ff264361a8b99396fb08979cf18eab
rexijohn <rexikj@gmail.com>
2020-06-10 22:28:11 -0700

    hello

--- src/pck1/cl1.java
+++ src/pck1/cl1.java
@@ -3,11 +3,6 @@ package pck1;
 //Adding from local branch
 //Added from Second Branch
 //Adding from remote server to second branch
-<<<<<<< HEAD
-//Care
-=======
-//Changes are made to see remote sync
->>>>>>> 560445c7d7844a886f87b89debaebbd21e2def59
 public class cl1 {
 
 	public static void main(String[] args) {
